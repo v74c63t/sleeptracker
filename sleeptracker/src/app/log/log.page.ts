906ba3e4d403b2d17864cedb1194c717afbbfbdd
v74c63t <vanesst5@uci.edu>
2023-03-01 22:13:59 -0800

    Added sleepiness slider/button functions

--- sleeptracker/src/app/log/log.page.ts
+++ sleeptracker/src/app/log/log.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SleepData } from '../data/sleep-data';
+import { OvernightSleepData } from '../data/overnight-sleep-data';
+import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
 
 @Component({
   selector: 'app-log',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log.page.scss'],
 })
 export class LogPage implements OnInit {
-
+  logSleepiness:boolean = false;
+  logOvernightSleep:boolean = false;
+  ScaleValues = StanfordSleepinessData.ScaleValues;
+  sleepinessRangeVal:number=1;
+  
   constructor() { }
 
   ngOnInit() {
   }
-
+  sleepinessClick(){
+    this.logSleepiness = !this.logSleepiness;
+  }
+  overnightClick() {
+    this.logOvernightSleep = !this.logOvernightSleep;
+  }
 }
