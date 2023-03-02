c334749454c8f38af9383e019c3027f832ac0392
v74c63t <vanesst5@uci.edu>
2023-03-02 13:54:51 -0800

    Tried getting data in view

--- sleeptracker/src/app/view/view.page.ts
+++ sleeptracker/src/app/view/view.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SleepData } from '../data/sleep-data';
+import { OvernightSleepData } from '../data/overnight-sleep-data';
+import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
+import { LogPage } from '../log/log.page'; 
 
 @Component({
   selector: 'app-view',
@@ -6,10 +10,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view.page.scss'],
 })
 export class ViewPage implements OnInit {
+  data:SleepData[] = [];
 
-  constructor() { }
+  constructor(private logPage:LogPage) { }
 
   ngOnInit() {
+    this.data = this.logPage.getSleepData();
+    console.log(this.data);
   }
 
 }
