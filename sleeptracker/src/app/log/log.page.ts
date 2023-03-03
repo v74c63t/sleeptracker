bf89bedc8a7230a3614d55edfa85e1c47fe0e59e
v74c63t <vanesst5@uci.edu>
2023-03-02 17:54:25 -0800

    Added card modal for confirmation

--- sleeptracker/src/app/log/log.page.ts
+++ sleeptracker/src/app/log/log.page.ts
@@ -31,20 +31,33 @@ export class LogPage implements OnInit {
   }
   sleepinessClick(){
     this.logSleepiness = !this.logSleepiness;
-    this.reset();
+    this.resetSleepiness();
   }
   sleepinessCancel(){
     this.logSleepiness = false;
-    this.reset();
+    this.resetSleepiness();
   }
-  reset(){
+  resetSleepiness(){
     this.sleepinessRangeVal = 1;
     this.resetCalDate();
   }
   resetCalDate(){
     this.resetDate = (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().slice(0, -1);
     this.sleepinessDate = this.resetDate;
+  }
+  overnightCancel() {
+    this.resetCalStartDate();
+    this.resetCalEndDate();
+    this.fullEnd = false;
+    this.fullStart = false; 
+    this.overnightLog = false;
+  }
+  resetCalStartDate(){
+    this.resetDate = (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().slice(0, -1);
     this.overnightStart = this.resetDate;
+  }
+  resetCalEndDate(){
+    this.resetDate = (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().slice(0, -1);
     this.overnightEnd = this.resetDate;
   }
   overnightClick() {
@@ -53,16 +66,16 @@ export class LogPage implements OnInit {
   createSleepinessData() {
     this.data = new StanfordSleepinessData(this.sleepinessRangeVal, new Date(this.sleepinessDate)); 
   }
-  deleteSleepinessData() {
+  deleteData() {
     this.data = new SleepData;
   }
-  sleepiness() {
-    // this.data = new StanfordSleepinessData(this.sleepinessRangeVal); 
-    this.sleepData.push(this.data);
-    console.log(this.data.summaryString());
-    console.log(this.sleepData.length);
-    console.log(this.sleepinessDate);
-  }
+  // sleepiness() {
+  //   // this.data = new StanfordSleepinessData(this.sleepinessRangeVal); 
+  //   this.sleepData.push(this.data);
+  //   console.log(this.data.summaryString());
+  //   console.log(this.sleepData.length);
+  //   console.log(this.sleepinessDate);
+  // }
   logFull(){
     this.fullStart = !this.fullStart;
   }
@@ -71,6 +84,20 @@ export class LogPage implements OnInit {
   }
   startLog() {
     this.overnightLog = !this.overnightLog;
+  }
+  createOvernightData() {
+    console.log(this.overnightStart);
+    console.log(this.overnightEnd);
+    this.data = new OvernightSleepData(new Date(this.overnightStart), new Date(this.overnightEnd));
+  }
+  addData() {
+    this.sleepData.push(this.data);
+    console.log(this.data.summaryString());
+    console.log(this.sleepData.length);
+    // console.log(this.sleepinessDate);
+  }
+  printDate() {
+    console.log(this.overnightEnd);
   }
   getSleepData(){
     return this.sleepData;
