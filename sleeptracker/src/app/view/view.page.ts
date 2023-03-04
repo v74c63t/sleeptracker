d6ef910698d7ca0abe2fa92b42018b7b70838334
v74c63t <vanesst5@uci.edu>
2023-03-03 18:12:26 -0800

    Displayed data in list/added alert for deletion

--- sleeptracker/src/app/view/view.page.ts
+++ sleeptracker/src/app/view/view.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SleepData } from '../data/sleep-data';
 import { OvernightSleepData } from '../data/overnight-sleep-data';
 import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
-import { LogPage } from '../log/log.page'; 
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-view',
@@ -10,14 +10,90 @@ import { LogPage } from '../log/log.page';
   styleUrls: ['./view.page.scss'],
 })
 export class ViewPage implements OnInit {
-  data:SleepData[] = [];
+  sleepData:SleepData[] = [];
+  delete:boolean = false;
 
-  constructor(){}
+  constructor(private alertController: AlertController){}
 
   ngOnInit() {
-    this.data = JSON.parse(localStorage.getItem('SleepData')||"null");
+    var data = JSON.parse(localStorage.getItem('SleepData')||"null");
     //FIGURE OUT HOW TO GET THE DATA
-    console.log(this.data);
+    // console.log(data);
+    // console.log(data[0].type);
+    // // console.log()
+    // console.log(typeof(data));
+    if(data != null) {
+      this.convert(data);
+    }
+      
+    // console.log(this.sleepData);
   }
-
+  convert(data:any[]) {
+    console.log("hello?")
+    console.log(data.length);
+    for(var i=0; i< data.length; i++) {
+      console.log(data[i].type);
+      console.log(data[i].type.valueOf() === "Stanford");
+      // console.log(typeof(data[i].type), typeof("Stanford"));
+    //   for (var index = 0; index < data[i].type.length; ++index) {
+    //     console.log("char " + index + ": " + data[i].type.charCodeAt(index));
+    //     console.log("char " + index + ": " + "Stanford".charCodeAt(index));
+    // }
+      if(data[i].type === "Stanford") {
+        console.log("in");
+        this.sleepData.push(new StanfordSleepinessData(data[i].loggedValue, new Date(data[i].loggedAt)));
+      }
+      else if(data[i].type === "Overnight") {
+        console.log("in");
+        this.sleepData.push(new OvernightSleepData(new Date(data[i].sleepStart), new Date(data[i].sleepEnd)));
+      }
+    }
+  }
+  clear() {
+    localStorage.clear();
+    this.sleepData = [];
+  }
+  getDataIndex(data:SleepData) {
+    for(var i = 0; i < this.sleepData.length; i++) {
+      if(data.id == this.sleepData[i].id) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  remove(data:SleepData) {
+    var i = this.getDataIndex(data);
+    console.log(i);
+    if(i != -1) {
+      this.sleepData.splice(i, 1);
+      localStorage.setItem("SleepData", JSON.stringify(this.sleepData));
+    }
+    console.log(this.sleepData);
+    this.delete = false;
+  }
+  startDateTimeString(data:SleepData) {
+    return data.dateTimeString().substring(0,data.dateTimeString().search("\n"))
+  }
+  endDateTimeString(data:SleepData) {
+    return data.dateTimeString().substring(data.dateTimeString().search("\n")+1,);
+  }
+  turnOffDel() {
+    this.delete = false;
+  }
+  async confirmDelete(data:SleepData) {
+    const alert = await this.alertController.create({
+      header: 'Logs cannot be recovered once deleted. If you are sure press delete again to delete the log after closing the alert.',
+      buttons: [
+        {
+          text: 'Ok',
+          role: 'delete'
+        }
+      ]
+    });
+    await alert.present();
+    const { role } = await alert.onDidDismiss();
+    if(role == 'delete') {
+      this.delete = true; 
+    }
+  }
 }
